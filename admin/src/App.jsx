41b949489ee26288fa0c9c579cd01762e902c51c
C. Venkareswara Reddy <125543441+venkateswarareddychalla@users.noreply.github.com>
2025-08-20 09:57:52 +0530

    Update App.jsx with render url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import Orders from "./pages/Orders/Orders";
  import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-yczg.onrender.com";
   return (
     <div>
       <ToastContainer />
